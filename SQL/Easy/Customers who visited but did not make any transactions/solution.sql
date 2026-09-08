b32481ad9c8e5d4b1483d324aa71570a06c5bf17
Sudharsan Balajee <jacksonjay451@gmail.com>
2026-09-08 11:41:36 +0530

    customers who visited but did not make any transactions

---
+++ SQL/Easy/Customers who visited but did not make any transactions/solution.sql
@@ -0,0 +1,1 @@
+SELECT Visits.customer_id, COUNT(Visits.customer_id) AS count_no_trans FROM Visits LEFT JOIN Transactions ON Visits.visit_id = Transactions.visit_id WHERE Transactions.transaction_id IS NULL GROUP BY Visits.customer_id;
